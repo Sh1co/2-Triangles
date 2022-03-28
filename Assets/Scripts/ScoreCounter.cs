95619c9b3b8ea0053de37f892defc0a7aac6b46e
Sherif Nafee <shico.m.nafee@gmail.com>
2022-03-28 03:20:01 +0300

    Make scoring system

---
+++ Assets/Scripts/ScoreCounter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using UnityEngine;
+
+public class ScoreCounter : MonoBehaviour
+{
+    public Action<int> ScoreUpdated;
+
+    private void Start()
+    {
+        _highScore = PlayerPrefs.GetInt("HighScore", 0);
+    }
+
+    public void Add(int value)
+    {
+        _score += value;
+        if (_score <= _highScore) return;
+        _highScore = _score;
+        PlayerPrefs.SetInt("HighScore", _highScore);
+    }
+
+    public void ResetScore()
+    {
+        _score = 0;
+    }
+
+    private int _score = 0;
+    private int _highScore = 0;
+}
